e6f4ce6b3be1a11e0109add3a6281e786097a41c
Amit <amit007kolambikar@gmail.com>
2021-06-27 15:52:36 +0700

    3

--- js/custom.js
+++ js/custom.js
@@ -32,24 +32,24 @@ function drawChart() {
   chart.draw(data, options);
 }
 
-(function () {
-  const second = 1000,
-        minute = second * 60,
-        hour = minute * 60,
-        day = hour * 24;
+// (function () {
+//   const second = 1000,
+//         minute = second * 60,
+//         hour = minute * 60,
+//         day = hour * 24;
 
-  let birthday = "June 27, 2021 13:00:00",
-      countDown = new Date(birthday).getTime(),
-      x = setInterval(function() {    
+//   let birthday = "June 27, 2021 13:00:00",
+//       countDown = new Date(birthday).getTime(),
+//       x = setInterval(function() {    
 
-        let now = new Date().getTime(),
-            distance = countDown - now;
+//         let now = new Date().getTime(),
+//             distance = countDown - now;
 
-        document.getElementById("days").innerText = Math.floor(distance / (day)),
-          document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
-          document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
-          document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+//         document.getElementById("days").innerText = Math.floor(distance / (day)),
+//           document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+//           document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+//           document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
 
-        //seconds
-      }, 0)
-  }());
+//         //seconds
+//       }, 0)
+//   }());
